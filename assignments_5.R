278f6fa8eb8faa4bdb6f32a0daf96c84ce8e8fbe
Esko Lehtonen <eplehton@turquino>
2016-08-24 15:14:06 +0300

    Assignments

---
+++ assignments_5.R
@@ -0,0 +1,41 @@
+#
+# Exercises
+# 
+
+
+##################
+# 1) Take a closer look to the t-test command. Use the parameters to calculate
+#
+# a) Change the confidence level used to 99%
+# b) Perform one-sided test.
+# c) By default, R does not assume equal variance. Therefore the results says it is Welch t-test. 
+#    How can you change this?
+
+##################
+# 2) Instead of t.test, try to use non-parametric alternative. Hint: ?wilcox.test
+
+
+#################
+# 3) Load the population data again (Population_in_Finland.csv) and divide the municipalities according to their size and
+# population change (increasing, decreasing). 
+#
+# Hint: If you have completed the assignment, it is easy to use source to load it again. 
+# Hint: Especially, if you have used good variables names which do not conflict with other
+# variable names in the script.
+
+#################
+# 4) Make boxplots which describe the available variables. 
+
+#################
+# 5) Make boxplots which describe the population changes for largish (>50 000) 
+# and small municipalities. 
+
+#################
+# 6) Test if the difference is statistically significant (p < .05). Which test would you use?
+#
+
+
+
+
+
+
---
+++ assignments_6.R
@@ -0,0 +1,51 @@
+#
+# Exercises
+#
+
+#####
+# 1) Load dataset sleep from the R dataset repository using command data(sleep).
+#    Get accustomed to the data. What are the variables? What do they represent?
+
+
+#####
+# 2) Create different kinds of plots, using both boxplot and ggplot2, to describe the
+# differences between the two drugs. 
+
+######
+# 3) Test the difference between the two drugs to extra hours of sleep using 
+# Student's paired t-test.
+
+### 
+# 4) The data is in long format. Reshape the data to wide format, so the two observations
+# from the same participant (denoted with variable group) are on their own variables. 
+
+###
+# 5) Create a scatter plot where the extra hours for drug 1 are on the x-axis, and
+# the extra hours for drug 2 are on the y-axis. 
+
+
+####
+# 6) Load the game data from file kr_gameraw.txt. 
+#
+# Calculate the accuracy of responses for each participant. See the lecture notes (rmANOVA.html) 
+# how to calculate it. 
+
+####
+# 7) Make a plot which describes the level of accuracy by age group and gender. 
+# Run ANOVA 2 x 2 to investigate if there are differences in accuracy as a function of age group or gender, 
+# weather there is interaction of age group and gender?
+
+####
+# 8) In the lecture notes the game was divided to three sets, and then we investigated the learning 
+# over the game. Perform a repeates measures ANOVA to investigate if the gender have 
+# any significant effect on learning. This means that you need to investigate the interaction
+# of set and gender. 
+
+####
+# 9) Refresh your memories from previous statistics courses. 
+# Is everything right the repeated measures ANOVA you just performed? 
+# Are we breaking any assumptions?
+
+
+
+
