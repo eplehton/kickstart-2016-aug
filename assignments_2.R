148e0d7b03209f54a7153942d6f838753d0a0122
MarkusTMattsson <markus.mattsson@helsinki.fi>
2016-08-24 17:57:42 +0300

    updated assignment 2

--- assignments_2.R
+++ assignments_2.R
@@ -3,8 +3,8 @@
 # 
 
 ##############################
-# 1) Load data from file titanic_colnames.txt to a data frame. Hint: Use function read.table
-
+# 1) Load data from file "titanic_colnames.txt" to a data frame. 
+# Hint: Use the function "read.table"
 
 
 #############################
@@ -13,16 +13,28 @@
 
 
 # Do you see any difference between the two data frames?
-# - column names
-# - row number column as data
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #############################
-# 3) The second data frame does not have meaningful columns names, because they were 
-# missing from the file. For the sake of an exercise, please set the column names 
+# 3) The second data frame does not have meaningful column names, because there were
+# none in the text file. For the sake of an exercise, please set the column names 
 # but try to avoid writing the columns names by hand. Instead, try to copy the values from the
 # first data frame. 
 
-# Hint! Use function names to investigate the data frames. Pay attention to the details. 
+# Hint! Use the function "names" to investigate the data frames. Pay attention to the details. 
+# What happens if we run this command?
 
 titanic2 <- read.table("titanic_nocolnames.txt", sep=';', header=TRUE)
 names(titanic2)
@@ -31,20 +43,22 @@ names(titanic2)[2:6]
 # Do you see what's happening?
 
 
+
+
 #############################
-# 4) Investigate the variables in another of the data frames. 
+# 4) Investigate the variables in one of the data frames. 
 #
-# Which variables are factors and which are not?
-# 
+# Which variables are of type "factor" and which are not?
+
 # Is there a function to check this? Hint: ?factor
 #
-# Is some of the factors ordered? Can you change some of them to ordered?
-
+# Is any of the factors ordered? Can you change one of them to be ordered? For which variable does
+# this make sense?
 
 
 
 ###############################
-# 5) Load the academics data from academics_data.R. This is slightly modified 
+# 5) Load the academics data from academics_data.R. This is a slightly modified 
 # version of the data displayed during the lecture. 
 # Hint: ?source 
 
@@ -63,8 +77,6 @@ names(titanic2)[2:6]
 
 
 ####################################
-# 8) In the assignments 1.1.1. you created coffee data frame. Recreate the data frame and write it 
-# to a text file. This will be used later. 
-
-
+# 8) In In assignment 1,  you created a data frame called "coffee". 
+# Recreate the data frame and write it to a text file. This file will be used later. 
 
--- assignments_2_with_answers.R
+++ assignments_2_with_answers.R
@@ -3,9 +3,13 @@
 # 
 
 ##############################
-# 1) Load data from file titanic_colnames.txt to a data frame. Hint: Use function read.table
+# 1) Load data from file "titanic_colnames.txt" to a data frame. 
+# Hint: Use the function "read.table"
 
-titanic <- read.table("titanic_colnames.txt", header = TRUE, sep=":") #NB: Working directory needs to be set correctly
+titanic <- read.table("titanic_colnames.txt", header = TRUE, sep=":") 
+
+#NB: Working directory needs to be set correctly
+
 titanic
 
 #############################
@@ -15,24 +19,30 @@ titanic.nocolnames <- read.table("titanic_nocolnames.txt", header = FALSE, sep="
 titanic.nocolnames
 
 # Do you see any difference between the two data frames?
-# - column names
-# - row number column as data
 
+head(titanic)
+head(titanic.nocolnames)
 
+# - column names: the first data frame has meaningful column names (variable names)
+# - row number column as data: the second data frame has row numbers both in the usual
+# row.names variable and the V1 variable
 
 #############################
-# 3) The second data frame does not have meaningful columns names, because they were 
-# missing from the file. For the sake of an exercise, please set the column names 
+# 3) The second data frame does not have meaningful column names, because there were
+# none in the text file. For the sake of an exercise, please set the column names 
 # but try to avoid writing the columns names by hand. Instead, try to copy the values from the
 # first data frame. 
 
-# Hint! Use function names to investigate the data frames. Pay attention to the details. 
+# Hint! Use the function "names" to investigate the data frames. Pay attention to the details. 
+# What happens if we run this command?
 
 titanic2 <- read.table("titanic_nocolnames.txt", sep=';', header=TRUE)
+
 # Doing this takes the variable names from the first line of data, which is incorrect in this case
 names(titanic2)
 names(titanic2)[2:6]
 names(titanic)
+
 # Do you see what's happening?
 
 # The mistake can be noted by comparing the two datasets:
@@ -45,11 +55,22 @@ head(titanic2)
 head(titanic.nocolnames)
 
 #############################
-# 4) Investigate the variables in another of the data frames. 
+# 4) Investigate the variables in one of the data frames. 
 #
-# Which variables are factors and which are not?
+# Which variables are of type "factor" and which are not?
 
-for (i in 1:length(names(titanic))) {print(class(titanic[[i]]))}
+# I'm using a simple for-loop below to answer the question. The syntax is surely intelligible
+# for those who have any programming experience, but for those without, a brief explanation
+# may be useful. For-loops (and other loops) repeat command a certain number of times, each
+# time changing something in the code automatically. In this case, the index "i" starts from
+# 1 and goes through the consecutive vaues until the end of the vector "names(titanic)". This
+# basic logic is declared inside the first pair of brackets. Then, inside the curly brackets
+# I'm telling the computer what I'd want it to do: in this case, I'd like it to just print out
+# the class attribute of each variable in the "titanic" data frame. 
+
+for (i in 1:length(names(titanic))) {
+  print(class(titanic[[i]]))
+}
 
 # [1] "factor"
 # [1] "factor"
@@ -59,10 +80,18 @@ for (i in 1:length(names(titanic))) {print(class(titanic[[i]]))}
 
 # 
 # Is there a function to check this? Hint: ?factor
-#
-# Is some of the factors ordered? Can you change some of them to ordered?
 
-for (i in 1:length(names(titanic))) {print(is.ordered(titanic[[i]]))}
+?factor
+
+# The help file tells us that there is indeed a function called "is.factor()" that we can use 
+# to check whether something is a factor. The function returns "TRUE" or "FALSE". 
+#
+# Is any of the factors ordered? Can you change one of them to be ordered? For which variable does
+# this make sense?
+
+for (i in 1:length(names(titanic))) {
+  print(is.ordered(titanic[[i]]))
+}
 
 # None are ordered
 
@@ -75,7 +104,7 @@ for (i in 1:length(names(titanic))) {print(is.ordered(titanic[[i]]))}
 # That did the trick!
 
 ###############################
-# 5) Load the academics data from academics_data.R. This is slightly modified 
+# 5) Load the academics data from "academics_data.R". This is a slightly modified 
 # version of the data displayed during the lecture. 
 # Hint: ?source 
 
@@ -89,9 +118,9 @@ academics
 # b) Using comma (,) as a separator
 # c) Without column names
 
-academics1 <- write.table(academics,"academics1.txt", sep = " ")
-academics2 <- write.table(academics,"academics2.txt", sep = ",")
-academics3 <- write.table(academics,"academics3.txt", col.names = FALSE)
+write.table(academics, "academics1.txt", sep = " ")
+write.table(academics, "academics2.txt", sep = ",")
+write.table(academics, "academics3.txt", col.names = FALSE)
 
 ###############################
 # 7) Read academics data back from the text files you just created in exercise 6. 
@@ -101,8 +130,8 @@ academics <- read.table("academics1.txt", header=TRUE, sep = " ")
 academics
 
 ####################################
-# 8) In the assignments 1.1.1. you created coffee data frame. Recreate the data frame and write it 
-# to a text file. This will be used later. 
+# 8) In assignment 1 you created a data frame called "coffee". 
+# Recreate the data frame and write it to a text file. This file will be used later. 
 
 coffee
 write.table(coffee, "coffee.txt",row.names = FALSE)
